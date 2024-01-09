45a94701c6b81d5916342f2b7079894cd395d453
Samuel <Samuel@DESKTOP-A3SKA1I>
2024-01-09 11:11:17 -0800

    initiate commit

--- src/tutour/School.java
+++ src/tutour/School.java
@@ -3,6 +3,11 @@ package tutour;
 public class School {
 	public static void main(String[] args) {
 		System.out.println("edris ali");
+		
+		System.out.println("zip code 121321");
+		
 	}
 // comment 
+	
+	
 }
